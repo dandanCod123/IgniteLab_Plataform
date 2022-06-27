597ba170f4d507249634787d9758e347cde95585
dandanCod123 <danielmouradesousa5@gmail.com>
2022-06-27 13:41:29 -0300

    finalização-01

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,7 +1,7 @@
 import { CheckCircle} from 'phosphor-react'
 import {isPast, format} from 'date-fns'
 import ptBr from 'date-fns/locale/pt-BR'
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 interface LessonProps {
     title:string;
@@ -11,8 +11,12 @@ interface LessonProps {
 }
 
 export function Lesson(props: LessonProps){
+    const {slug} = useParams<{slug:string}>()
+    
     const isLessonAvailable  = isPast(props.awailableAt);
     const availableFormatted = format(props.awailableAt, "EEEE' . 'd' de 'MMMM' . 'k'h'mm", {locale: ptBr,})
+
+    const isActiveLesson = slug === props.slug;
 
     return(
     <Link to={`/event/lesson/${props.slug}`} className="group">
@@ -21,16 +25,27 @@ export function Lesson(props: LessonProps){
         </span>
        
        {/** div-os quadrados dos conteudos, spans-> textos e ao vivo */}
-       <div className="rounded border border-gray-500 p-4  mt-2 group-hover:border-green-500">
+       <div className={`rounded border border-gray-500 p-4  mt-2 group-hover:border-green-500
+        ${isActiveLesson ? `bg-green-500`: ``}
+       `}>
            <header className="flex items-center justify-between">
 
             {isLessonAvailable ? (
-                <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+
+                <span className={`text-sm  font-medium flex items-center gap-2
+                ${isActiveLesson ? `text-white`: ``}
+                ${!isActiveLesson ? `text-blue-500`:``}
+                `}
+                >
                 <CheckCircle size={20}/>
                 Conteudo liberado
             </span>
+
             ) : (
-                <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+                <span className={`text-sm  font-medium flex items-center gap-2
+                ${isActiveLesson ? `text-white`: ``}
+                ${!isActiveLesson ? `text-orange-500`:``}
+                `}>
                  <CheckCircle size={20}/>
                  Em breve
              </span>
@@ -41,7 +56,9 @@ export function Lesson(props: LessonProps){
                  </span>
            </header>
 
-           <strong className="text-gray-200 mt-5 block">
+           <strong className={`t mt-5 block 
+           ${isActiveLesson ? `text-white`: ``}
+           ${!isActiveLesson ? `text-gray-200` : ``}`}>
              {props.title}
            </strong>
        </div>
